a2693eb720b1fe50071c7dc53f944c0d679f8b82
Jesse Stone <jstone@psi.edu>
2020-01-09 13:59:39 -0700

    Added code to download PDS3 context objects

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 
 wget -r -N --no-host-directories --no-parent https://pds.nasa.gov/data/pds4/context-pds4/ -A '*.xml'
+wget -r -N --no-host-directories --no-parent https://pds.nasa.gov/data/pds4/context-pds3/ -A '*.xml'
