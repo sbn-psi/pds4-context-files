c5df22c39069e2cc463db08ca6dc7ff555f22652
Jesse Stone <jstone@psi.edu>
2020-09-24 13:26:32 -0700

    20200924 update

--- download.sh
+++ download.sh
@@ -3,5 +3,5 @@
 wget -r -N --no-host-directories --no-parent https://pds.nasa.gov/data/pds4/context-pds4/ -A '*.xml'
 wget -r -N --no-host-directories --no-parent https://pds.nasa.gov/data/pds4/context-pds3/ -A '*.xml'
 
-find data/pds4/context-pds4 -name "*.xml" -exec grep "<logical_identifier>" '{}' \; | sed -e 's/<logical_identifier>//' -e 's~</logical_identifier>~~' -e 's~ *~~'
+find data/pds4/context-pds4 -name "*.xml" -exec grep "<logical_identifier>" '{}' \; | sed -e 's/<logical_identifier>//' -e 's~</logical_identifier>~~' -e 's~ *~~' >  pds4lids.txt
 
